bf61760b24bd92c78b90a20112971d28794a08eb
Nay Zaw Aung <nayzawaung750@gmail.com>
2023-02-17 22:36:31 +0630

    php lesson 5 speration of concern

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Learngin</title>
-</head>
-<style>
-    body{
-        background-color: gray;
-        padding: 2em;
-        text-align: center;
-    }
-</style>
-<body>
-<h1>
-    <!-- <?php
-    
-    //echo "Hello ". $_GET['name'];
-    ?> -->
-    <?="Hello " . htmlspecialchars($_GET['name']); ?>
-</h1>
-</body>
-</html>
+<?php
+$greeting = "Hello World";
+
+require "index.view.php";
