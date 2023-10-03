cb9e30a695100d1ced7dc2ceb797fbe75c11e282
nayzawaung750nza <nayzawaung750@gmail.com>
2023-10-03 10:29:09 +0630

    php lesson 16 class basic concept and thinking

--- functions.php
+++ functions.php
@@ -3,3 +3,26 @@ function dd($data){
     echo "<pre>";
     die(var_dump($data));
 }
+
+class Car{
+    public static function startEngine(){//static method not allow use for $this keyword
+        echo "Car Start Engine.<br>";
+        return new Car;
+    }
+    public function drive(){
+        echo "Car is driving.<br>";
+        return new Car;
+    }
+    public function stop(){
+        echo "Car stopped.<br>";
+    }
+    public function whatisthis(){
+        return $this;
+    }
+}
+Car::startEngine()->drive()->stop();//for used static function
+
+$car = new Car();
+$car->drive();
+$car->stop();
+print_r($car->whatisthis()->stop());
